0cfd2ad140d2ef7a3b6f175075840e9684ce72ca
Dale Wang <dalinwanglhtz@gmail.com>
2020-01-13 21:48:21 +1100

    first working cut

--- src/com/codewardev/Finance.java
+++ src/com/codewardev/Finance.java
@@ -1,12 +1,17 @@
 package com.codewardev;
 
 import java.math.BigInteger;
+import java.util.stream.IntStream;
 
 public class Finance {
 
 	public static BigInteger finance(int n) {
-		
-		return null;
+		long total = 0;
+		int tmp = n+1;
+		for(int i=0; i<=n*2; i+=2) {
+			total += IntStream.range(i, tmp++).sum();
+		}
+		return BigInteger.valueOf(total);
 	}
 
 }
